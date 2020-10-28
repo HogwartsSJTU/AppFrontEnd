deed5c931d9ff8ea7b76c8a00b931c0bbe887561
Shang-QY <55442231+Shang-QY@users.noreply.github.com>
2020-10-28 14:16:57 +0800

    好友列表风格适配

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,8 @@ import 'package:flutter/services.dart';
 import 'package:Hogwarts/theme/app_theme.dart';
 import 'package:Hogwarts/component/custom_drawer/navigation_home_screen.dart';
 
-
+// TODO LIST
+// TODO 联系我们；登录注册；个人中心；个人信息维护；浏览；设置；颜色语言设置；路由控制；游记管理；组队管理；主页
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SystemChrome.setPreferredOrientations(<DeviceOrientation>[DeviceOrientation.portraitUp, DeviceOrientation.portraitDown])
@@ -23,13 +24,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Freelancer',
       debugShowCheckedModeBanner: false,
-//      theme: ThemeData(
-//        primarySwatch: Colors.blue,
-//        textTheme: AppTheme.textTheme,
-//        platform: TargetPlatform.iOS,
-//      ),
       theme: ThemeData(
-        primaryColor: Colors.red,
+        primarySwatch: Colors.blue,
         accentColor: Color(0xFFFEF9EB),
         textTheme: AppTheme.textTheme,
         platform: TargetPlatform.iOS,
